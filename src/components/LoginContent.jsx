e39ad9b73c720d09476798b29cd1aee1f4338447
Arv-78 <valorant8228@gmail.com>
2022-11-10 17:43:12 +0530

    add style

--- src/components/LoginContent.jsx
+++ src/components/LoginContent.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-import Box from '@mui/material/Box';
-import loginContentImg from "../images/loginContent.png"
+import Box from "@mui/material/Box";
+import loginContentImg from "../images/loginContent.png";
 
-function LoginContent(){
-    return(
-        <Box 
-        display="flex"
-        height="100vh"
-        justifyContent="center"
-        sx={{
-            backgroundColor: '#4F37E9'  
-          }}>
-        <img className="login-content-Img" src={loginContentImg} alt="loginContentImg"/>
-        </Box>
-    )
+function LoginContent() {
+  return (
+    <Box
+      display="flex"
+      height="100vh"
+      justifyContent="center"
+      sx={{
+        backgroundColor: "#4F37E9",
+      }}
+    >
+      <img
+        className="login-content-Img"
+        src={loginContentImg}
+        alt="loginContentImg"
+      />
+    </Box>
+  );
 }
 
 //this is devO1
